917d7a9d0809a59beb804b332e9b57258f754a4b
zarrine905 <83436425+zarrine905@users.noreply.github.com>
2021-05-11 20:20:46 +0530

    Add files via upload

---
+++ vcftobedintersect_WithoutPandas.py
@@ -0,0 +1,58 @@
+#To intersect .vcf with .bed without using pandas 
+#11/05/21
+
+f = open("WithoutpandasResult.vcf", "w")
+
+#To read columns of vcf into list
+f = open("example.vcf","r")
+lines = f.readlines()
+#print(lines[0])
+vpos = []
+rowlist = []
+header = ""
+for vline in lines:
+	if ("#" in vline):
+		header += vline + "\n"
+	elif ("#" not in vline):
+		#vpos.append(vline)
+		#print (line)
+		vline = vline.split()	#To read columns of vcf 
+		vpos.append(vline[1])
+		rowlist.append(vline)   #To read lines/rows of vcf into list
+
+print(header)
+#f.write(header)
+#print(vpos)
+#print(rowlist[0])
+
+
+#To read columns of .bed into list
+fh = open("example.bed", "r")
+blines = fh.readlines()
+#print(blines)
+
+bstart=[]
+bend =[]
+for bline in blines:
+	bline = bline.split()
+	#print(bline[2])
+	bstart.append(bline[1])
+	bend.append(bline[2])
+#print(bstart)
+#print(bend)
+
+
+
+for i in range(len(vpos)):
+	for j in range(len(bstart)):
+		if ((vpos[i] >= bstart[j]) and (vpos[i]<= bend[j])):
+			list_val = rowlist[i]
+			myjoin = "\t".join(map(str, list_val))
+			print(myjoin)
+			#f.write(myjoin)
+			
+		
+
+
+
+
